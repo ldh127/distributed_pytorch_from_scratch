0c8a4870aadd626acb844ac92d59573a3f527092
jujianzhong <jujianzhong@xiaomi.com>
2025-03-31 06:04:56 +0000

    Add dataset.py

---
+++ dataset.py
@@ -0,0 +1,63 @@
+import os
+import json
+from functools import partial
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import PreTrainedTokenizerFast
+
+from constants import BOS_TOKEN, EOS_TOKEN, UNK_TOKEN
+
+
+class ShakespeareDataset(Dataset):
+    def __init__(self, data_path: str, tokenizer_path: str, split: str, maxlen):
+        assert split in ["train", "val"], f"Expectected split to be 'train' or 'val', but got {split}"
+        assert os.path.exists(data_path) and os.path.exists(tokenizer_path)
+        with open(data_path, 'r') as f:
+            self.data = json.load(f)[split]
+        self.maxlen = maxlen
+        self.tokenizer = PreTrainedTokenizerFast(tokenizer_file=tokenizer_path)
+        self.tokenizer.bos_token = BOS_TOKEN
+        self.tokenizer.eos_token = EOS_TOKEN
+        self.tokenizer.unk_token = UNK_TOKEN
+        self.bos = self.tokenizer.bos_token_id
+        self.eos = self.tokenizer.eos_token_id
+        self.unk = self.tokenizer.unk_token_id
+        self.vocab_size = len(self.tokenizer)
+        self.split = split
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        tokens = self.tokenizer(self.data[idx], return_tensors='pt')['input_ids'][0]
+        tokens = tokens[:self.maxlen]       # clip to maxlen
+        return tokens     # (seq_len,)
+
+
+def collate_fn(batch, bos: int, eos: int, ignore_idx: int):
+    max_len = max(len(x) for x in batch)
+    input_ids = torch.full((len(batch), max_len + 1), fill_value=eos, dtype=torch.long)
+    target_ids = torch.full((len(batch), max_len + 1), fill_value=ignore_idx, dtype=torch.long)
+    for i, b in enumerate(batch):
+        input_ids[i, 0] = bos
+        input_ids[i, 1: len(b) + 1] = b
+        target_ids[i, :len(b)] = b
+        target_ids[i, len(b)] = eos
+    position_ids = torch.arange(max_len + 1, device=batch[0].device).unsqueeze(0).expand(len(batch), -1)
+    
+    return {
+        'input_ids': input_ids,
+        'target_ids': target_ids,
+        'position_ids': position_ids,
+    }
+
+
+def get_dataloader(
+    data_path: str, tokenizer_path: str, 
+    batch_size: int, ignore_idx: int, split: str, maxlen: int, shuffle: bool = True
+):
+    dataset = ShakespeareDataset(data_path, tokenizer_path, split, maxlen=maxlen)
+    collate_fn_partial = partial(collate_fn, bos=dataset.bos, eos=dataset.eos, ignore_idx=ignore_idx)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn_partial)
+    return dataloader
