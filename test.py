086ae6afe9cdfe43f3c3cdcd2f9a8b01f88d88db
jujianzhong <jujianzhong@xiaomi.com>
2025-04-01 03:24:46 +0000

    add test.py. A minor version

---
+++ test.py
@@ -0,0 +1,132 @@
+import os
+import re
+import glob
+import argparse
+from dataclasses import asdict
+
+import tqdm
+import torch
+import torch.multiprocessing as mp
+import torch.nn.functional as F
+
+import process_manager as pm
+from models.model import Transformer, VallinaTransformer
+from dataset import get_dataloader
+from constants import IGNORE_INDEX
+from constants import ModelArgumments
+from utils import init_dist_env, set_seed
+
+
+def get_test_args():
+    parser = argparse.ArgumentParser()
+    
+    group = parser.add_argument_group("distributed")
+    group.add_argument('--master_addr', type=str, default='localhost')
+    group.add_argument('--master_port', type=str, default='23333')
+    group.add_argument('--tp_size', type=int, default=2)
+
+    group = parser.add_argument_group("data")
+    group.add_argument("--data_path", "-d", type=str, required=True)
+    group.add_argument("--tokenizer_path", "-t", type=str, required=True)
+    
+    group = parser.add_argument_group("model")
+    group.add_argument("--use_vallina_impl", action='store_true')
+    parser.add_argument("--ckpt_dir", type=str, required=True)
+
+    group = parser.add_argument_group("other")
+    group.add_argument("--random_seed", type=int, default=0)
+    
+    args = parser.parse_args()
+    return args
+
+
+def test(rank, args):
+    set_seed(args.random_seed)
+    init_dist_env(args, rank)
+    
+    model_cls = VallinaTransformer if args.use_vallina_impl else Transformer
+    model_args = ModelArgumments()
+    model = model_cls(**asdict(model_args))
+    model.cuda()
+    model.reset_parameters()        # re-initialize parameters (neccassary for tensor-parallel Transformer)
+    model.eval()
+
+    ckpt_paths = glob.glob(os.path.join(args.ckpt_dir, f'tprank-{pm.pgm.tp_rank}_iter-*_loss-*.pth'))
+    ckpt_paths = sorted(ckpt_paths, key=lambda x: int(re.findall(r"tprank-\d+_iter-(\d+)_loss-.*?.pth", x)[0]))
+    if len(ckpt_paths) == 0:
+        raise ValueError(f"[TP Rank {pm.pgm.tp_rank}]: No checkpoints found in {args.ckpt_dir}")
+    print(f"[TP Rank {pm.pgm.tp_rank}]: Found {len(ckpt_paths)} checkpoints. Loading the latest one: {ckpt_paths[-1]}")
+    ckpt_path = ckpt_paths[-1]
+    ckpt = torch.load(ckpt_path, map_location='cuda')
+    model.load_state_dict(ckpt)
+
+    batch_size = 1
+    dataloader = get_dataloader(
+        args.data_path, args.tokenizer_path, batch_size, IGNORE_INDEX, 
+        split='validation', maxlen=model_args.maxlen, shuffle=False,
+    )
+
+    # calc validation loss
+    pbar = tqdm.tqdm(range(len(dataloader)), desc=f"[TP Rank {pm.pgm.tp_rank}]: calc validation loss", position=pm.pgm.tp_rank)
+    accum_loss = 0.
+    for batch in dataloader:
+        input_ids = batch['input_ids'].cuda()
+        target_ids = batch['target_ids'].cuda()
+        position_ids = batch['position_ids'].cuda()
+        with torch.inference_mode():
+            logits = model(input_ids, position_ids)
+        loss = F.cross_entropy(
+            logits.view(-1, logits.size(-1)), target_ids.view(-1), 
+            ignore_index=IGNORE_INDEX, reduction='mean',
+        )
+        accum_loss += loss.item()
+        pbar.update(1)
+        pbar.set_postfix({'avg_loss': accum_loss / pbar.n})
+    pbar.close()
+
+    avg_loss = accum_loss / len(dataloader.dataset)
+    print(f"[TP Rank {pm.pgm.tp_rank}]: Validation loss: {avg_loss}")
+    save_path = os.path.join(args.ckpt_dir, "val", f'tprank-{pm.pgm.tp_rank}_val.txt')
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    if pm.pgm.tp_rank == 0:
+        with open(save_path, 'w') as f:
+            f.write(f"Validation loss: {avg_loss}\n")
+
+    # continue writing (greedy decoding)
+    texts = [
+        "Nice to meet you, ",
+        "Great empire never falls, ",
+        "Your majesty, it's my duty ",
+        "I shall be glad ",
+        "What a glory to "
+    ]
+    decoded = []
+    tokenizer = dataloader.dataset.tokenizer
+    bos_id = dataloader.dataset.bos
+    eos_id = dataloader.dataset.eos
+    for t in texts:
+        t = t.strip()
+        tokens = tokenizer(t, return_tensors='pt')['input_ids'].cuda()    # (1, seq_len)
+        tokens = F.pad(tokens, (1, 0), mode="constant", value=bos_id)
+        while True:
+            position_ids = torch.arange(tokens.size(-1), dtype=torch.long, device=tokens.device).unsqueeze(0)    # (1, seq_len)
+            with torch.inference_mode():
+                logits = model(tokens, position_ids)[0, -1]     # (vocab_size,)
+            pred_token = logits.argmax(dim=-1).item()
+            tokens = F.pad(tokens, (0, 1), mode="constant", value=pred_token)   # (1, seq_len + 1)
+            if tokens[0, -1].item() == eos_id:
+                trans = tokenizer.decode(tokens[0, 1:-1].tolist(), clean_up_tokenization_spaces=True).strip()
+                assert t in trans, f"Prediction {trans} does not contain the input text {t}"
+                decoded.append((t, trans[len(t):]))
+                break
+    
+    if pm.pgm.tp_rank == 0:
+        with open(save_path, 'a') as fp:
+            for input_text, decoded_text in decoded:
+                print(f"{input_text} -> {decoded_text}")
+                print(f"{input_text} -> {decoded_text}", file=fp)
+
+
+if __name__ == '__main__':
+    args = get_test_args()
+    mp.spawn(test, args=(args, ), nprocs=args.tp_size, join=True)
