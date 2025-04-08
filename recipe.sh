1e3d09ca09eb0425443981c66092b09ef255a451
xiabingquan <53991699+xiabingquan@users.noreply.github.com>
2025-04-08 22:52:35 +0800

    Update recipe.sh

--- recipe.sh
+++ recipe.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-txt_data_path="pg100.txt"
-json_data_path="pg100.json"
+ori_data_path=".../fineweb/CC-MAIN-2023-50/000_00000.parquet"
+json_data_path="data.json"
 token_data_path="${json_data_path%.*}_tokens.json"
-
 vocab=1024
 tokenizer_path="./tokenizer/tokenizer.json"
 root_ckpt_dir="./.checkpoints"  
 
 
 step_idx=1
-echo -e "Step ${step_idx}: Downloading txt data...\n"
-if [ ! -f "${txt_data_path}" ]; then
-    echo "Downloading ${txt_data_path}..."
-    corpus_url="https://www.gutenberg.org/cache/epub/100/pg100.txt"
-    wget "${corpus_url}"
+echo -e "Step ${step_idx}: Downloading raw data...\n"
+if [ ! -f "${ori_data_path}" ]; then
+    echo "Downloading ${ori_data_path}..."
+    data_url="https://huggingface.co/datasets/HuggingFaceFW/fineweb/resolve/main/data/CC-MAIN-2023-50/000_00000.parquet?download=true"
+    curl -L "${data_url}" -o "${ori_data_path}"
 else
-    echo -e "txt data path exisits, skip downloading\n"
+    echo -e "raw data path exisits, skip downloading\n"
 fi
 
 
 step_idx=$((step_idx+1))
 echo -e "Step ${step_idx}: Preprocessing data...\n"
 if [ ! -f "${json_data_path}" ]; then
-    echo "Preprocessing ${txt_data_path}..."
-    python3 ./preprocess_data.py "${txt_data_path}" "${json_data_path}"
+    echo "Preprocessing ${ori_data_path}..."
+    python3 ./preprocess_data.py "${ori_data_path}" "${json_data_path}"
 else
     echo -e "json data path exisits, skip preprocessing\n"
 fi
@@ -55,70 +54,72 @@ ckpt_dir="${root_ckpt_dir}/tp1"
 if [ ! -d "${ckpt_dir}" ]; then
     echo "Training TP-1 (Vallina) model..."
     CUDA_VISIBLE_DEVICES='7' python3 ./train.py --tp_size 1 --save_dir "${ckpt_dir}" --data_path "${token_data_path}" \
-     --master_addr "localhost" --max_steps 50000 --reserv_last_n_ckpts -1 --save_interval 1000 --master_port 23330 --bf16
+     --master_addr "localhost" --master_port 23330 --bf16
 else
     echo -e "Vallina model ckpt exisits, skip training\n"
 fi
 
+
 step_idx=$((step_idx+1))
-echo -e "Step ${step_idx}: Test TP-1 (Vallina) model...\n"
-ckpt_dir="${root_ckpt_dir}/tp1_10k_iter"
+echo -e "Step ${step_idx}: Train TP-2 model...\n"
+ckpt_dir="${root_ckpt_dir}/tp2"
+if [ ! -d "${ckpt_dir}" ]; then
+    echo "Training TP-2 model..."
+    CUDA_VISIBLE_DEVICES='4,5' python3 ./train.py --tp_size 2 --save_dir "${ckpt_dir}" --data_path "${token_data_path}" \
+     --master_addr "localhost" --master_port 23331 --bf16
+else
+    echo -e "TP-2 model ckpt exisits, skip training\n"
+fi
+
+
+step_idx=$((step_idx+1))
+echo -e "Step ${step_idx}: Train TP-4 model...\n"
+ckpt_dir="${root_ckpt_dir}/tp4"
+if [ ! -d "${ckpt_dir}" ]; then
+    echo "Training TP-4 model..."
+    CUDA_VISIBLE_DEVICES='0,1,2,3' python3 ./train.py --tp_size 4 --save_dir "${ckpt_dir}" --data_path "${token_data_path}" \
+     --master_addr "localhost" --master_port 23332 --bf16
+else
+    echo -e "TP-4 model ckpt exisits, skip training\n"
+fi
+
+
+ step_idx=$((step_idx+1))
+ echo -e "Step ${step_idx}: Test TP-1 (Vallina) model...\n"
+ ckpt_dir="${root_ckpt_dir}/tp1"
+ save_dir="${ckpt_dir}/val"
+ if [ ! -d "${save_dir}" ]; then
+     echo "Testing TP-1 (Vallina) model..."
+     CUDA_VISIBLE_DEVICES='7' python3 ./test.py --tp_size 1 --ckpt_dir "${ckpt_dir}" \
+      --data_path "${token_data_path}" --tokenizer_path "${tokenizer_path}" \
+      --master_addr "localhost" --master_port 23330
+ else
+     echo -e "TP-1 (Vallina) model test result exisits, skip testing\n"
+ fi
+
+
+ step_idx=$((step_idx+1))
+ echo -e "Step ${step_idx}: Test TP-2 model...\n"
+ ckpt_dir="${root_ckpt_dir}/tp2"
+ save_dir="${ckpt_dir}/val"
+ if [ ! -d "${save_dir}" ]; then
+     echo "Testing TP-2 model..."
+     CUDA_VISIBLE_DEVICES='0,1' python3 ./test.py --tp_size 2 --ckpt_dir "${ckpt_dir}" \
+      --data_path "${token_data_path}" --tokenizer_path "${tokenizer_path}"
+ else
+     echo -e "TP-2 model test result exisits, skip testing\n"
+ fi
+
+
+step_idx=$((step_idx+1))
+echo -e "Step ${step_idx}: Test TP-4 model...\n"
+ckpt_dir="${root_ckpt_dir}/tp4"
 save_dir="${ckpt_dir}/val"
-
-echo "Testing TP-1 (Vallina) model..."
-CUDA_VISIBLE_DEVICES='0' python3 ./test.py --tp_size 1 --ckpt_dir "${ckpt_dir}" \
- --data_path "${token_data_path}" --tokenizer_path "${tokenizer_path}"
-# if [ ! -d "${save_dir}" ]; then
-#     echo "Testing TP-1 (Vallina) model..."
-#     CUDA_VISIBLE_DEVICES='0' python3 ./test.py --tp_size 1 --ckpt_dir "${ckpt_dir}" \
-#      --data_path "${token_data_path}" --tokenizer_path "${tokenizer_path}"
-# else
-#     echo -e "TP-1 (Vallina) model test result exisits, skip testing\n"
-# fi
-
-# step_idx=$((step_idx+1))
-# echo -e "Step ${step_idx}: Test TP-1 (Vallina) model...\n"
-# ckpt_dir="${root_ckpt_dir}/tp1"
-# save_dir="${ckpt_dir}/val"
-# if [ ! -d "${save_dir}" ]; then
-#     echo "Testing TP-1 (Vallina) model..."
-#     CUDA_VISIBLE_DEVICES='0' python3 ./test.py --tp_size 1 --ckpt_dir "${ckpt_dir}" \
-#      --data_path "${token_data_path}" --tokenizer_path "${tokenizer_path}"
-# else
-#     echo -e "TP-1 (Vallina) model test result exisits, skip testing\n"
-# fi
-
-# step_idx=$((step_idx+1))
-# echo -e "Step ${step_idx}: Train TP-2 model...\n"
-# ckpt_dir="${root_ckpt_dir}/tp2"
-# if [ ! -d "${ckpt_dir}" ]; then
-#     echo "Training TP-2 model..."
-#     CUDA_VISIBLE_DEVICES='2,3' python3 ./train.py --tp_size 2 --save_dir "${ckpt_dir}" --data_path "${token_data_path}" \
-#      --master_addr "localhost" --master_port 23331 --bf16
-# else
-#     echo -e "TP-2 model ckpt exisits, skip training\n"
-# fi
-
-# step_idx=$((step_idx+1))
-# echo -e "Step ${step_idx}: Test TP-2 model...\n"
-# ckpt_dir="${root_ckpt_dir}/tp2"
-# save_dir="${ckpt_dir}/val"
-# if [ ! -d "${save_dir}" ]; then
-#     echo "Testing TP-2 model..."
-#     CUDA_VISIBLE_DEVICES='5,7' python3 ./test.py --tp_size 2 --ckpt_dir "${ckpt_dir}" \
-#      --data_path "${token_data_path}" --tokenizer_path "${tokenizer_path}"
-# else
-#     echo -e "TP-2 model test result exisits, skip testing\n"
-# fi
-
-
-# step_idx=$((step_idx+1))
-# echo -e "Step ${step_idx}: Train TP-4 model...\n"
-# ckpt_dir="${root_ckpt_dir}/tp4"
-# if [ ! -d "${ckpt_dir}" ]; then
-#     echo "Training TP-4 model..."
-#     CUDA_VISIBLE_DEVICES='4,5,6,7' python3 ./train.py --tp_size 4 --save_dir "${ckpt_dir}" --data_path "${token_data_path}" \
-#      --master_addr "localhost" --master_port 23332 --bf16
-# else
-#     echo -e "TP-4 model ckpt exisits, skip training\n"
-# fi
+if [ ! -d "${save_dir}" ]; then
+    echo "Testing TP-4 model..."
+    CUDA_VISIBLE_DEVICES='2,3,4,5' python3 ./test.py --tp_size 4 --ckpt_dir "${ckpt_dir}" \
+     --data_path "${token_data_path}" --tokenizer_path "${tokenizer_path}" \
+     --master_addr "localhost" --master_port 23332
+else
+    echo -e "TP-4 model test result exisits, skip testing\n"
+fi
